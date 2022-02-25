fcf11a7adb750c557a65aa32ba90e44ec8f97889
EvelynGriffith <70599987+EvelynGriffith@users.noreply.github.com>
2022-02-25 13:01:09 -0500

    fixing the words

--- search/search/main.py
+++ search/search/main.py
@@ -1,7 +1,6 @@
 """Find, open, and read file to gather a specific text."""
 
 from pathlib import Path
-from pickletools import string1
 
 from rich.console import Console
 
@@ -12,19 +11,16 @@ cli = typer.Typer()
 def confirm_valid_file(file: Path) -> bool:
     """Confirm that the provided file is a valid path."""
     # determine if the file is not None and if it is a file
-    path_to_file = 'input/proactive.txt'
-    path = Path(path_to_file)
-    path.is_file()
     # return a value to indicate if the file is valid
     # by checking that the is_file() function returns True
     if file is not None:
         if file.is_file():
             return True
+        else:
+            return False
     # This function should return True when the file is
     # not None and it is also a valid file. Otherwise, this
     # function should return False to indicate it is not value.
-    return False
-
 
 def human_readable_boolean(answer: bool) -> str:
     """Produce a human-readable Yes or No for a boolean value of True or False."""
@@ -38,13 +34,20 @@ def human_readable_boolean(answer: bool) -> str:
 
 def word_search(text: str, word: str) -> bool:
     """Determine whether or no.. a word is found in the text in case-sensitive fashion."""
-    # perform a case-sensitive search for the word in the provided text
-
+    # perform a case-sensitive search for the word in5 the provided text
     # text = file1.read()
-    if word in text:
-        return True
-    else:
-        return False
+    # if text.index(word):
+    all_words = text.split(" ")
+    for element in all_words:
+        if element is word:
+            return True
+    return False
+    # for element in text.splitlines(' '):
+    #     print(element)
+    #     if element == word:   
+    #         return True
+    #     else:
+    #         return False
 
 @cli.command()
 def word(
@@ -65,20 +68,23 @@ def word(
     # confirm the file is valid and so the program should search through it for the word
     # --> read in the contents of the file
     # --> read in the contents of the file
-    confirmed_file = confirm_valid_file(file)
+    confirmed_file = confirm_valid_file(file_fully_qualified)
     # --> search for the word in the contents of the file by calling function
-    file_string = open(file_fully_qualified, "r")
-    # file_string = file_fully_qualified.read()
+    file = open(file_fully_qualified, "r")
+    file_string = file.read()
+    # print(file_string)
     words = word_search(file_string, word)
     call_function = human_readable_boolean(words)
     # --> display a message about whether the word was or was not found
     # since the file was not valid and thus you cannot install it display a message
-    console.print()
-    console.print(f"😃 Searching through the file called {file_string}!")
-    console.print()
-    console.print(f"Was the word '{word}' found in the file input/proactive.txt? {call_function}")
-    console.print()
-    console.print(f"😃 Searching through the file called input/notfound.txt!")
-    console.print()
-    console.print(f"🤷 input/notfound.txt was not a valid file")
-    console.print()
+    if confirmed_file is True:
+        console.print()
+        console.print(f"😃 Searching through the file called input/proactive.txt!")
+        console.print()
+        console.print(f"Was the word '{word}' found in the file input/proactive.txt? {call_function}")
+    else: 
+        console.print()
+        console.print(f"😃 Searching through the file called input/notfound.txt!")
+        console.print()
+        console.print(f"🤷 input/notfound.txt was not a valid file")
+        console.print()
